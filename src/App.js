f577fc44b600ba035da54bd0af6fd8fdf6286834
Carlynn-Adams <carlynnadams25@gmail.com>
2024-06-28 13:11:28 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="https://github.com/Carlynn-Adams" target="_blank" rel="noreferrer">Carlynn Adams</a> and is{" "}
-        <a
-          href="https://github.com/Carlynn-Adams/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Carlynn-Adams"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Carlynn Adams
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Carlynn-Adams/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
