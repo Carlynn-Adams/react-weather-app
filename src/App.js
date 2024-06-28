ebb1aeed53d828fe8daf0917ec58dce06444922a
Carlynn-Adams <carlynnadams25@gmail.com>
2024-06-28 11:49:06 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://github.com/Carlynn-Adams" target="_blank" rel="noreferrer">Carlynn Adams</a> and is{" "}
+        <a
+          href="https://github.com/Carlynn-Adams/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
